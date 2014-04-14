9963b0b4ab08720dfe4a6206ac31912926b35a99
ming.ma <ming.ma@upai.com>
2014-04-14 09:53:06 +0800

    补充文件：将 rose-bls 中的工具类 BeanProxy 迁移到 jocean-j2se 工程中，位于 org.jocean.j2se.spring 子包下

---
+++ src/main/java/org/jocean/j2se/spring/BeanProxy.java
@@ -0,0 +1,86 @@
+/**
+ * 
+ */
+package org.jocean.j2se.spring;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.FactoryBean;
+
+/**
+ * @author isdom
+ *
+ */
+public class BeanProxy<T> implements FactoryBean<T> {
+
+    private static final Logger LOG = 
+    	LoggerFactory.getLogger(BeanProxy.class);
+    
+	@SuppressWarnings("unchecked")
+    public BeanProxy(final Class<T> intf) {
+		this._intf = new Class[]{intf};
+		this._proxy = buildProxy();
+	}
+
+	@SuppressWarnings("unchecked")
+	public T buildProxy() {
+		return (T)Proxy.newProxyInstance(
+		        Thread.currentThread().getContextClassLoader(), 
+		        this._intf, 
+				new InvocationHandler() {
+                    @Override
+                    public Object invoke(
+                            final Object proxy, 
+                            final Method method,
+                            final Object[] args) throws Throwable {
+                        final T impl = _ref.get();
+                        if ( null != impl ) {
+                            return method.invoke(impl, args);
+                        }
+                        throw new RuntimeException("implementation Object !NOT! set yet.");
+                    }} );
+	}
+
+	public void setImpl(final T impl) {
+		if ( !this._ref.compareAndSet(null, impl) ) {
+		    LOG.warn("BeanProxy already set implementation, ignore {}", impl);
+		}
+	}
+
+	@Override
+	public String toString() {
+		final T impl = this._ref.get();
+		
+		if ( null != impl ) {
+			return impl.toString();
+		}
+		else {
+		    return	"BeanProxy with null impl!";
+		}
+	}
+
+    @Override
+    public T getObject() throws Exception {
+        return this._proxy;
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return this._intf[0];
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+
+    private final AtomicReference<T> _ref = new AtomicReference<T>(null);
+    private final Class<T>[]  _intf;
+    private final T _proxy;
+    
+}
