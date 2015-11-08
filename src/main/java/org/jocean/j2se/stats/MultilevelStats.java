94cdbbb17c4159f830ea5081a736add78c18a249
ming.ma <isdom.maming@gmail.com>
2015-11-08 20:18:39 +0800

    实现 fetchStatsSnapshot, 和 静态工厂方法

--- src/main/java/org/jocean/j2se/stats/MultilevelStats.java
+++ src/main/java/org/jocean/j2se/stats/MultilevelStats.java
@@ -7,4 +7,10 @@ public interface MultilevelStats {
     public void recordInterval(final long interval, final Object ...levels);
     
     public <K,V> Map<K,V> fetchStatsSnapshot(final Object ...levels);
+    
+    public static class Util {
+        public static MultilevelStats buildStats(final int levels) {
+            return new MultilevelStatsImpl(levels);
+        }
+    }
 }
--- src/main/java/org/jocean/j2se/stats/MultilevelStatsImpl.java
+++ src/main/java/org/jocean/j2se/stats/MultilevelStatsImpl.java
@@ -6,7 +6,7 @@ import org.jocean.idiom.Function;
 import org.jocean.idiom.SimpleCache;
 import org.jocean.idiom.stats.TimeIntervalMemo;
 
-public class MultilevelStatsImpl implements MultilevelStats{
+class MultilevelStatsImpl implements MultilevelStats{
     
     MultilevelStatsImpl(final int levels) {
         if (levels <= 0) {
@@ -33,14 +33,31 @@ public class MultilevelStatsImpl implements MultilevelStats{
         int idx = 0;
         SimpleCache<Object, Object> cache = this._cache;
         while ( idx < levels.length - 1 ) {
-            cache = (SimpleCache<Object, Object>)cache.get(levels[idx++]);
+            final Object value = cache.get(levels[idx++]);
+            if ( !(value instanceof SimpleCache)) {
+                throw new RuntimeException("internal error for more levels");
+            }
+            cache = (SimpleCache<Object, Object>)value;
         }
-        final TimeIntervalMemo memo = (TimeIntervalMemo)cache.get(levels[idx]);
-        memo.recordInterval(interval);
+        final Object value = cache.get(levels[idx]);
+        if ( !(value instanceof TimeIntervalMemo)) {
+            throw new RuntimeException("internal error for less levels");
+        }
+        ((TimeIntervalMemo)value).recordInterval(interval);
     }
     
+    @SuppressWarnings("unchecked")
     public <K,V> Map<K,V> fetchStatsSnapshot(final Object ...levels) {
-        return null;
+        int idx = 0;
+        SimpleCache<Object, Object> cache = this._cache;
+        while ( idx < levels.length ) {
+            final Object value = cache.get(levels[idx++]);
+            if ( !(value instanceof SimpleCache)) {
+                throw new RuntimeException("internal error for more levels");
+            }
+            cache = (SimpleCache<Object, Object>)value;
+        }
+        return (Map<K,V>)cache.snapshot();
     }
     
     private final SimpleCache<Object, Object> _cache;
