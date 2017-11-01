8554d877493716638c1f0cb74170bffb54da0ec7
ming.ma <isdom.maming@gmail.com>
2017-11-01 10:16:39 +0800

    用 Beans.fromString 替换 getAsType(value, type)

--- src/main/java/org/jocean/j2se/spring/FieldValueSetter.java
+++ src/main/java/org/jocean/j2se/spring/FieldValueSetter.java
@@ -1,10 +1,9 @@
 package org.jocean.j2se.spring;
 
-import java.beans.PropertyEditor;
-import java.beans.PropertyEditorManager;
 import java.lang.reflect.Field;
 import java.util.Properties;
 
+import org.jocean.idiom.Beans;
 import org.jocean.idiom.ExceptionUtils;
 import org.jocean.idiom.ReflectUtils;
 import org.slf4j.Logger;
@@ -18,24 +17,6 @@ public class FieldValueSetter implements BeanPostProcessor {
     private static final Logger LOG = 
             LoggerFactory.getLogger(FieldValueSetter.class);
 
-    @SuppressWarnings("unchecked")
-    private static <T> T getAsType(final String value, final Class<T> type) {
-        if (type.equals(String.class)) {
-            return (T)value;
-        } else {
-            final PropertyEditor editor = PropertyEditorManager.findEditor(type);
-            if (null != editor) {
-                editor.setAsText(value);
-                return (T)editor.getValue();
-            } else {
-                LOG.warn("can't found PropertyEditor for type{}, skip get value {}.",
-                        type, value);
-                throw new RuntimeException(
-                    "can't found PropertyEditor for type ("+ type +")");
-            }
-        }
-    }
-    
     public FieldValueSetter(final Properties properties) {
         this._properties = properties;
     }
@@ -52,7 +33,7 @@ public class FieldValueSetter implements BeanPostProcessor {
                     final String value = this._properties.getProperty(key);
                     
                     if (null!=value) {
-                        field.set(bean, getAsType(value, field.getType()));
+                        field.set(bean, Beans.fromString(value, field.getType()));
                         if (LOG.isInfoEnabled()) {
                             LOG.info("set value {} to bean({})'s field({}) by key {}", value, beanName, field, key);
                         }
