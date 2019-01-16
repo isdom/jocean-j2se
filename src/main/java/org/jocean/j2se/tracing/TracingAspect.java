e488eba519fe42e332179b61b9572edb0aab4775
ming.ma <isdom@qq.com>
2019-01-16 16:45:41 +0800

    传递被 around 切面 方法的返回值

--- src/main/java/org/jocean/j2se/tracing/TracingAspect.java
+++ src/main/java/org/jocean/j2se/tracing/TracingAspect.java
@@ -10,17 +10,19 @@ public class TracingAspect {
 
     private static final Logger LOG = LoggerFactory.getLogger(TracingAspect.class);
 
-    public void around(final ProceedingJoinPoint pjp) throws Throwable {
+    public Object around(final ProceedingJoinPoint pjp) throws Throwable {
         final Object[] args = pjp.getArgs();
         if (args.length > 0 && args[0] instanceof Tracing) {
             final Tracing tracing = (Tracing) args[0];
             try (final Scope scope = tracing.activate()) {
                 final Object ret = pjp.proceed();
-                LOG.info("pjp.proceed() within tracing's return value is {}", ret);
+                LOG.debug("pjp.proceed() within tracing's return value is {}", ret);
+                return ret;
             }
         } else {
             final Object ret = pjp.proceed();
-            LOG.info("pjp.proceed() without tracing's return value is {}", ret);
+            LOG.debug("pjp.proceed() without tracing's return value is {}", ret);
+            return ret;
         }
     }
 }
