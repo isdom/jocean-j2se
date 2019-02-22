4c06e408fec65e20de918b53df0a6929dcb51c1f
ming.ma <isdom@qq.com>
2019-02-22 21:51:17 +0800

    初步实现 按照 host 加载特定配置信息

--- src/main/java/org/jocean/j2se/cli/cmd/StartAppCommand.java
+++ src/main/java/org/jocean/j2se/cli/cmd/StartAppCommand.java
@@ -1,5 +1,7 @@
 package org.jocean.j2se.cli.cmd;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.io.StringReader;
 import java.util.Date;
 import java.util.Properties;
@@ -10,14 +12,22 @@ import org.jocean.cli.CliContext;
 import org.jocean.j2se.AppInfo;
 import org.jocean.j2se.ModuleInfo;
 import org.jocean.j2se.os.OSUtil;
+import org.jocean.j2se.unit.UnitAgent;
+import org.jocean.j2se.unit.UnitAgentMXBean.UnitMXBean;
+import org.jocean.j2se.unit.model.ServiceConfig;
+import org.jocean.j2se.unit.model.UnitDescription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
 
 import com.alibaba.edas.acm.ConfigService;
+import com.google.common.base.Charsets;
+import com.google.common.collect.Maps;
 
 public class StartAppCommand implements CliCommand<CliContext> {
 
@@ -61,6 +71,29 @@ public class StartAppCommand implements CliCommand<CliContext> {
         final String appConfig = ConfigService.getConfig(appName, args[4], 6000);
         if (null != appConfig) {
             LOG.debug("{}/{} app config: {}", hostname, appName, appConfig);
+            final Yaml yaml = new Yaml(new Constructor(ServiceConfig[].class));
+
+            try (final InputStream is = new ByteArrayInputStream(appConfig.getBytes(Charsets.UTF_8))) {
+                final ServiceConfig[] confs = (ServiceConfig[])yaml.load(is);
+                for (final ServiceConfig conf : confs) {
+                    if (conf.getHost().equals(hostname)) {
+                        LOG.debug("found app-conf {} match hostname: {}", conf, hostname);
+                        final AbstractApplicationContext appctx = new ClassPathXmlApplicationContext("unit/clibooter.xml");
+                        final AppInfo app = appctx.getBean("appinfo", AppInfo.class);
+                        if (null != app) {
+                            for (final String lib : this._libs) {
+                                app.getModules().put(lib, new ModuleInfo(lib));
+                            }
+                        }
+
+                        build(appctx.getBean(UnitAgent.class), conf.getConf(), null);
+                        this._ctxRef.set(appctx);
+
+                        return "OK";
+                    }
+                }
+            }
+            LOG.debug("NOT found any app-conf match hostname: {}", hostname);
         } else {
             LOG.debug("{}/{} app config is null", hostname, appName);
         }
@@ -89,6 +122,52 @@ public class StartAppCommand implements CliCommand<CliContext> {
         }
 
         return "OK";
+    }
+
+    private void build(final UnitAgent unitAgent, final UnitDescription desc, final String parentPath) {
+        final String pathName = null != parentPath
+                ? parentPath + desc.getName()
+                : desc.getName();
+        final String template = getTemplateFromName(desc.getName());
+        final Properties properties = desc.parametersAsProperties();
+        final String[] source = genSourceFrom(properties);
+
+        UnitMXBean unit = null;
+        if (null != source ) {
+            unit = unitAgent.createUnitWithSource(
+                    pathName,
+                    source,
+                    Maps.fromProperties(properties));
+        } else {
+            unit = unitAgent.createUnit(
+                    pathName,
+                    new String[]{"**/"+ template + ".xml", template + ".xml"},
+                    Maps.fromProperties(properties),
+                    true);
+        }
+        if (null == unit) {
+            LOG.info("create unit {} failed.", pathName);
+        } else {
+            LOG.info("create unit {} success with active status:{}", pathName, unit.isActive());
+        }
+        for ( final UnitDescription child : desc.getChildren()) {
+            build(unitAgent, child, pathName + "/");
+        }
+    }
+
+    private static final String SPRING_XML_KEY = "__spring.xml";
+
+    private static String[] genSourceFrom(final Properties properties) {
+        final String value = properties.getProperty(SPRING_XML_KEY);
+        properties.remove(SPRING_XML_KEY);
+        return null!=value ? value.split(",") : null;
+    }
+
+    private String getTemplateFromName(final String name) {
+      // for return value, eg:
+      //  and gateway.1.2.3.4 --> gateway
+      final int idx = name.indexOf('.');
+      return ( idx > -1 ) ? name.substring(0, idx) : name;
     }
 
     @Override
