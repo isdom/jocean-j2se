d6836d542c362a1b9174ca5c7b8499b4619b7dd0
ming.ma <isdom@qq.com>
2019-03-21 11:43:39 +0800

    clear code, using lambda expression

--- src/main/java/org/jocean/j2se/stats/ApiStats.java
+++ src/main/java/org/jocean/j2se/stats/ApiStats.java
@@ -21,10 +21,9 @@ import com.google.common.collect.Multimap;
 
 import rx.functions.Action1;
 import rx.functions.Action2;
-import rx.functions.Func1;
 
 public class ApiStats implements ApisMBean, MBeanRegisterAware {
-    
+
      private static final String FLOWS_OBJECTNAME_SUFFIX = "name=apis";
 
     private static final Comparator<String> DESC_COMPARATOR = new Comparator<String>() {
@@ -32,22 +31,21 @@ public class ApiStats implements ApisMBean, MBeanRegisterAware {
         public int compare(final String o1, final String o2) {
             return o2.compareTo(o1);
         }};
-        
+
     @Override
     public void setMBeanRegister(final MBeanRegister register) {
-        register.registerMBean(FLOWS_OBJECTNAME_SUFFIX, 
-                MBeanUtil.createAndConfigureMBean(this));
+        register.registerMBean(FLOWS_OBJECTNAME_SUFFIX,  MBeanUtil.createAndConfigureMBean(this));
         this._register = register;
     }
-        
+
     public void addApi(final String path, final String method) {
         this._apis.put(path, method);
     }
-    
+
     @Override
     public String[] getApisAsText() {
         final List<String> apis = new ArrayList<>();
-        for ( Map.Entry<String, Collection<String>> entry 
+        for ( final Map.Entry<String, Collection<String>> entry
                 : this._apis.asMap().entrySet()) {
             final StringBuilder sb = new StringBuilder();
             final String path = entry.getKey();
@@ -56,14 +54,14 @@ public class ApiStats implements ApisMBean, MBeanRegisterAware {
             sb.append("]");
             sb.append(entry.getKey());
             sb.append("-->");
-            for (String method : entry.getValue()) {
+            for (final String method : entry.getValue()) {
                 sb.append(method);
                 sb.append("/");
             }
             final AtomicInteger idx = new AtomicInteger(1);
             fetchExecutedInterval(path, new Action2<String, Action1<OnCounter>>() {
                 @Override
-                public void call(final String reason, Action1<OnCounter> memo) {
+                public void call(final String reason, final Action1<OnCounter> memo) {
                     sb.append('\n');
                     sb.append('\t');
                     sb.append("(" + Integer.toString(idx.getAndIncrement()) + ")." + reason + ":");
@@ -79,41 +77,41 @@ public class ApiStats implements ApisMBean, MBeanRegisterAware {
                 }});
             apis.add(sb.toString());
         }
-        
+
         final String[] apisAsArray = apis.toArray(new String[0]);
         Arrays.sort(apisAsArray, DESC_COMPARATOR);
         return apisAsArray;
     }
-    
+
     @Override
     public Map<String, String> getApiInfo() {
         final Map<String, String> infos = Maps.newHashMap();
-        
-        for ( Map.Entry<String, Collection<String>> entry 
+
+        for ( final Map.Entry<String, Collection<String>> entry
                 : this._apis.asMap().entrySet()) {
-            
+
             final StringBuilder sb = new StringBuilder();
-            for (String method : entry.getValue()) {
+            for (final String method : entry.getValue()) {
                 sb.append("/");
                 sb.append(method);
             }
             infos.put(entry.getKey(), sb.toString());
         }
-        
+
         return infos;
     }
-    
+
     @Override
     public Map<String, Map<String,Object>> getApiStats() {
         final Map<String, Map<String,Object>> apis = Maps.newHashMap();
-        for ( Map.Entry<String, Collection<String>> entry 
+        for ( final Map.Entry<String, Collection<String>> entry
                 : this._apis.asMap().entrySet()) {
             final String path = entry.getKey();
             final Map<String,Object> counters = Maps.newHashMap();
             apis.put(path, counters);
-            
+
             counters.put("_TOTAL_", getExecutedCount(path));
-            
+
             fetchExecutedInterval(path, new Action2<String, Action1<OnCounter>>() {
                 @Override
                 public void call(final String reason, final Action1<OnCounter> memo) {
@@ -126,51 +124,43 @@ public class ApiStats implements ApisMBean, MBeanRegisterAware {
                         }});
                 }});
         }
-        
+
         return apis;
     }
-    
+
     public int incExecutedCount(final String path) {
         return this._executedCounters.get(path).incrementAndGet();
     }
-    
+
     private int getExecutedCount(final String path) {
         return this._executedCounters.get(path).get();
     }
-    
+
     public void recordExecutedInterval(final String path, final String endreason, final long interval) {
         CURRENT_RECORDPATH.set(path);
         this._executedTIMemos.recordInterval(interval, path, endreason);
     }
-    
-    private void fetchExecutedInterval(final String path, 
+
+    private void fetchExecutedInterval(final String path,
             final Action2<String, Action1<OnCounter>> receptor) {
-        final Map<String, CounterableTIMemo> snapshot = 
+        final Map<String, CounterableTIMemo> snapshot =
                 this._executedTIMemos.fetchStatsSnapshot(path);
-        for (Map.Entry<String, CounterableTIMemo> entry : snapshot.entrySet()) {
+        for (final Map.Entry<String, CounterableTIMemo> entry : snapshot.entrySet()) {
             receptor.call(entry.getKey(), entry.getValue());
         }
     }
 
     private MBeanRegister _register;
-    private final SimpleCache<String, AtomicInteger> _executedCounters = new SimpleCache<>(
-            new Func1<String, AtomicInteger>() {
-        @Override
-        public AtomicInteger call(final String path) {
-            return new AtomicInteger(0);
-        }});
-    
-    private final MultilevelStats _executedTIMemos = MultilevelStats.Util.buildStats(2,
-        new Action2<Object, Object>() {
-            @Override
-            public void call(final Object key, final Object cacheOrTIMemo) {
-                if (cacheOrTIMemo instanceof TIMemoImplOfRanges) {
-                    _register.registerMBean(FLOWS_OBJECTNAME_SUFFIX 
-                            + ",path=" + CURRENT_RECORDPATH.get()
-                            + ",reason=" + key, 
-                        ((TIMemoImplOfRanges)cacheOrTIMemo).createMBean());
-                }
-            }});
+    private final SimpleCache<String, AtomicInteger> _executedCounters = new SimpleCache<>( path-> new AtomicInteger(0));
+
+    private final MultilevelStats _executedTIMemos = MultilevelStats.Util.buildStats(2, (key, cacheOrTIMemo) -> {
+            if (cacheOrTIMemo instanceof TIMemoImplOfRanges) {
+                _register.registerMBean(FLOWS_OBJECTNAME_SUFFIX
+                        + ",path=" + CURRENT_RECORDPATH.get()
+                        + ",reason=" + key,
+                    ((TIMemoImplOfRanges)cacheOrTIMemo).createMBean());
+            }
+        });
     private final Multimap<String, String> _apis = ArrayListMultimap.create();
     private static final ThreadLocal<String> CURRENT_RECORDPATH = new ThreadLocal<>();
 }
