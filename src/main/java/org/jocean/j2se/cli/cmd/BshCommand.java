4582380dbef60688c0c112c1089b6bf69f601405
ming.ma <isdom@qq.com>
2019-03-31 19:26:26 +0800

    添加 bsh 命令支持

--- src/main/java/org/jocean/j2se/cli/cmd/BshCommand.java
+++ src/main/java/org/jocean/j2se/cli/cmd/BshCommand.java
@@ -1,9 +1,17 @@
 package org.jocean.j2se.cli.cmd;
 
+import java.io.StringReader;
+
 import org.jocean.cli.CliCommand;
 import org.jocean.cli.CliContext;
+import org.jocean.idiom.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.BaseEncoding;
+
+import bsh.Interpreter;
 
 public class BshCommand implements CliCommand<CliContext> {
 
@@ -11,7 +19,20 @@ public class BshCommand implements CliCommand<CliContext> {
 
     @Override
     public String execute(final CliContext ctx, final String... args) throws Exception {
-        return "OK";
+        final Interpreter inter = new Interpreter();
+//        inter.setOut(new PrintStream(outputStream));
+
+        if (args.length < 1) {
+            return "FAILED: missing script params\n" + getHelp();
+        }
+
+        try {
+            return inter.eval( new StringReader(new String(BaseEncoding.base64().decode(args[0]), Charsets.UTF_8)) ).toString();
+        }
+        catch (final Exception e) {
+            LOG.error("exception when inter.eval, detail: {}", ExceptionUtils.exception2detail(e));
+            return "Error: " + ExceptionUtils.exception2detail(e);
+        }
     }
 
     @Override
