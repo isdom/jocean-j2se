9ae32970d047b3c6a4c0336b88ae501cac518cc1
ming.ma <isdom@qq.com>
2019-07-05 17:29:38 +0800

    尝试匹配  #{ } 封装的 占位符

--- src/main/java/org/jocean/j2se/zk/UnitConfigOnZKUpdater.java
+++ src/main/java/org/jocean/j2se/zk/UnitConfigOnZKUpdater.java
@@ -82,7 +82,7 @@ public class UnitConfigOnZKUpdater implements MBeanRegisterAware {
 
         // for generate JSON format config
         if (config.isEmpty()) {
-            config = new PropertyPlaceholderHelper("${", "}").replacePlaceholders(null, this._template, placeholderResolver, null);
+            config = new PropertyPlaceholderHelper("#{", "}").replacePlaceholders(null, this._template, placeholderResolver, null);
         }
 
         try {
@@ -105,7 +105,7 @@ public class UnitConfigOnZKUpdater implements MBeanRegisterAware {
 
         // for generate JSON format config
         if (config.isEmpty()) {
-            config = new PropertyPlaceholderHelper("${", "}").replacePlaceholders(null, this._template, placeholderResolver, null);
+            config = new PropertyPlaceholderHelper("#{", "}").replacePlaceholders(null, this._template, placeholderResolver, null);
         }
 
         try {
