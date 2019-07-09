adf6ed14a62e958eb7b3003f205d6c65d2faffe5
ming.ma <isdom@qq.com>
2019-07-09 22:44:23 +0800

    在 clibooter 中加入 PrometheusMeterRegistry 的初始化

--- src/main/java/org/jocean/j2se/prometheus/JmxExporter.java
+++ src/main/java/org/jocean/j2se/prometheus/JmxExporter.java
@@ -1,6 +1,5 @@
 package org.jocean.j2se.prometheus;
 
-import java.io.File;
 import java.net.InetSocketAddress;
 
 import javax.inject.Inject;
@@ -9,21 +8,6 @@ import javax.management.ObjectName;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jmx.export.MBeanExporter;
 
-import io.github.mweirauch.micrometer.jvm.extras.ProcessMemoryMetrics;
-import io.github.mweirauch.micrometer.jvm.extras.ProcessThreadMetrics;
-import io.micrometer.core.instrument.Clock;
-import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
-import io.micrometer.core.instrument.binder.jvm.DiskSpaceMetrics;
-import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
-import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
-import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
-import io.micrometer.core.instrument.binder.logging.LogbackMetrics;
-import io.micrometer.core.instrument.binder.system.FileDescriptorMetrics;
-import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
-import io.micrometer.core.instrument.binder.system.UptimeMetrics;
-import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
-import io.micrometer.prometheus.PrometheusConfig;
-import io.micrometer.prometheus.PrometheusMeterRegistry;
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.exporter.HTTPServer;
 import io.prometheus.jmx.BuildInfoCollector;
@@ -37,35 +21,6 @@ public class JmxExporter {
 
         // JVM MBean Collector
         this._jmxCollector = new JmxCollector(_config).register(CollectorRegistry.defaultRegistry);
-
-        // MicroMeter Collector
-        if (null == _prometheusRegistry) {
-            _prometheusRegistry = new PrometheusMeterRegistry(
-                    PrometheusConfig.DEFAULT, CollectorRegistry.defaultRegistry, Clock.SYSTEM);
-
-            _compositeMeterRegistry.add(_prometheusRegistry);
-
-            _prometheusRegistry.config().commonTags(
-                    "application", System.getProperty("app.name"),
-                    "hostname", System.getenv("HOSTNAME"));
-
-            new ClassLoaderMetrics().bindTo(_prometheusRegistry);
-            new JvmMemoryMetrics().bindTo(_prometheusRegistry);
-            new JvmGcMetrics().bindTo(_prometheusRegistry);
-            new JvmThreadMetrics().bindTo(_prometheusRegistry);
-            new DiskSpaceMetrics(new File(System.getProperty("user.home"))).bindTo(_prometheusRegistry);
-//            new ExecutorServiceMetrics().bindTo(_prometheusRegistry);
-
-            new UptimeMetrics().bindTo(_prometheusRegistry);
-            new FileDescriptorMetrics().bindTo(_prometheusRegistry);
-            new ProcessorMetrics().bindTo(_prometheusRegistry);
-
-            // logback metrics
-            new LogbackMetrics().bindTo(_prometheusRegistry);
-
-            new ProcessMemoryMetrics().bindTo(_prometheusRegistry);
-            new ProcessThreadMetrics().bindTo(_prometheusRegistry);
-        }
 
         _httpserver = new HTTPServer(new InetSocketAddress(_port), CollectorRegistry.defaultRegistry);
 
@@ -90,13 +45,12 @@ public class JmxExporter {
     private JmxCollector _jmxCollector = null;
 
     static private BuildInfoCollector _buildInfoCollector = null;
-    static private PrometheusMeterRegistry _prometheusRegistry = null;
 
     @Inject
     MBeanExporter _mbeanExporter;
 
-    @Inject
-    CompositeMeterRegistry _compositeMeterRegistry;
+//    @Inject
+//    CompositeMeterRegistry _compositeMeterRegistry;
 
     @Value("${config}")
     String _config;
---
+++ src/main/java/org/jocean/j2se/prometheus/PrometheusUtil.java
@@ -0,0 +1,50 @@
+package org.jocean.j2se.prometheus;
+
+import java.io.File;
+
+import io.github.mweirauch.micrometer.jvm.extras.ProcessMemoryMetrics;
+import io.github.mweirauch.micrometer.jvm.extras.ProcessThreadMetrics;
+import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
+import io.micrometer.core.instrument.binder.jvm.DiskSpaceMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
+import io.micrometer.core.instrument.binder.logging.LogbackMetrics;
+import io.micrometer.core.instrument.binder.system.FileDescriptorMetrics;
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
+import io.micrometer.core.instrument.binder.system.UptimeMetrics;
+import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.prometheus.client.CollectorRegistry;
+
+public class PrometheusUtil {
+    public static void addPrometheusMeterRegistry(final CompositeMeterRegistry compositeMeterRegistry) {
+        final PrometheusMeterRegistry prometheusRegistry = new PrometheusMeterRegistry(
+                PrometheusConfig.DEFAULT, CollectorRegistry.defaultRegistry, Clock.SYSTEM);
+
+        compositeMeterRegistry.add(prometheusRegistry);
+
+        prometheusRegistry.config().commonTags(
+                "application", System.getProperty("app.name"),
+                "hostname", System.getenv("HOSTNAME"));
+
+        new ClassLoaderMetrics().bindTo(compositeMeterRegistry);
+        new JvmMemoryMetrics().bindTo(compositeMeterRegistry);
+        new JvmGcMetrics().bindTo(compositeMeterRegistry);
+        new JvmThreadMetrics().bindTo(compositeMeterRegistry);
+        new DiskSpaceMetrics(new File(System.getProperty("user.home"))).bindTo(compositeMeterRegistry);
+//        new ExecutorServiceMetrics().bindTo(_prometheusRegistry);
+
+        new UptimeMetrics().bindTo(compositeMeterRegistry);
+        new FileDescriptorMetrics().bindTo(compositeMeterRegistry);
+        new ProcessorMetrics().bindTo(compositeMeterRegistry);
+
+        // logback metrics
+        new LogbackMetrics().bindTo(compositeMeterRegistry);
+
+        new ProcessMemoryMetrics().bindTo(compositeMeterRegistry);
+        new ProcessThreadMetrics().bindTo(compositeMeterRegistry);
+    }
+}
