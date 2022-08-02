cdd40b0ca32d731f8d80611609053743a017b2b1
ming.ma <isdom@qq.com>
2022-08-02 21:35:00 +0800

    add ACM Demo

---
+++ src/test/java/org/jocean/j2se/mse/ACMDemo.java
@@ -0,0 +1,34 @@
+package org.jocean.j2se.mse;
+
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.alibaba.edas.acm.ConfigService;
+import com.alibaba.edas.acm.exception.ConfigException;
+
+public class ACMDemo {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ACMDemo.class);
+
+    public static void main(final String[] args) {
+        // 从控制台命名空间管理中拷贝对应值
+        final Properties props = new Properties();
+        props.put("endpoint", args[0]);
+        props.put("namespace", args[1]);
+        // 通过 ECS 实例 RAM 角色访问 ACM
+        props.put("ramRoleName", args[2]);
+
+        ConfigService.init(props);
+
+        try {
+            LOG.info("before ConfigService.getConfig");
+            final String strval = ConfigService.getConfig("demo", "demo_group", 6000);
+            LOG.info("===>: {}", strval);
+        } catch (final ConfigException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
